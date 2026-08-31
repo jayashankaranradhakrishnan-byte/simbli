4e7803d98c0a0f12be4bb711dbe965344378d8bd
Sri Jaya Shankaran Radha Krishnan <jayashankaran.radhakrishnan@dci.in>
2026-08-31 18:26:38 +0530

    landing page

--- pages/pricing_pom.ts
+++ pages/pricing_pom.ts
@@ -1,4 +1,4 @@
-import { Page, Locator } from '@playwright/test';
+import { Page, Locator, expect } from '@playwright/test';
 import { uivalidator } from '../pages/uivalidator';
 import { FooterPage } from '../pages/footer_pom';
 
@@ -13,6 +13,37 @@ export class PricingPage {
 
     get pricingpageHeaderContainer(): Locator {
         return this.page.getByText('Pricing', { exact: true });
+    }
+
+    get pricingpageHeader_badge(): Locator {
+        return this.page.locator('div.pricing-badge-pill');
+    }
+
+    get pricingpageHeader_h1(): Locator {
+        return this.page.getByRole('heading', { name: 'Simple pricing. Powerful AI.' });
+    }
+
+    get pricingpageHeader_h2(): Locator {
+        return this.page.locator('p.pricing-main-subtitle');
+    }
+
+
+
+    async pricing_page() {
+        await this.pricingpageHeaderContainer.click();
+
+        await expect(this.pricingpageHeader_badge).toBeVisible();
+
+        await expect(this.pricingpageHeader_h1).toBeVisible();
+        await expect(this.pricingpageHeader_h1)
+            .toHaveText('Simple pricing. Powerful AI.');
+
+        await expect(this.pricingpageHeader_h2).toBeVisible();
+        await expect(this.pricingpageHeader_h2)
+            .toHaveText(
+                'Start free and scale your AI capabilities as your business grows. Choose the plan that fits your needs and upgrade whenever you\'re ready.'
+            );
+
     }
 
 
--- tests/simbli.spec.ts
+++ tests/simbli.spec.ts
@@ -1,6 +1,7 @@
 import { test } from '../base/simbli_testbase';
 import { uivalidator } from '../pages/uivalidator';
 import { landingPage } from '../pages/landing_pom';
+import { PricingPage } from '../pages/pricing_pom';
 //import { setup, teardown, getPage } from '../base/simbli_testBase';
 
 test.describe('Simbli Test Suite', () => {
@@ -8,8 +9,10 @@ test.describe('Simbli Test Suite', () => {
     test('Simbli landing page test', async ({ page, request }) => {
         const simbli = new uivalidator(page, request);
         const landing = new landingPage(page, request);
+        const pricing = new PricingPage(page);
 
         await landing.landing_page()
+        await pricing.pricing_page();
 
     });
 
